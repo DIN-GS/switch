d81e735fa514540e87f63114252146e5a7b25078
DIN-GS <nikita.ivakin@nure.ua>
2022-10-19 18:51:43 +0300

    switch

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        Month month = Month.OCTOBER;
+
+
+        String season = switch (month){
+            case JUNE, JULY, AUGUST -> "Summer";
+            case FEBRUARY, JANUARY, DECEMBER -> "Winter";
+            case SEPTEMBER, OCTOBER, NOVEMBER -> "Autumn (Fall)";
+            case MARCH, APRIL, MAY -> "Spring";
+        };
+
+        System.out.println(season);
+    }
+}
